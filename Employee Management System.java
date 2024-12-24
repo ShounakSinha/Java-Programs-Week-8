55fa3f671ccaac48bc95408843822a49650d55db
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-12-24 22:27:09 +0530

    Update Employee Management System.java

--- Employee Management System.java
+++ Employee Management System.java
@@ -9,3 +9,24 @@
 // Developer: Adds programmingLanguage.
 // Override displayDetails() in each subclass to include additional details.
 // Write a main() method to demonstrate creating objects of each class and displaying their details.
+
+class Employee
+{
+    private String name, id;
+    private int salary=0;
+    Employee()
+    {
+        System.out.println("An object of Emloyee Class created");
+    }
+    void displayDetails()
+    {
+        System.out.println("The Employee Name is : "+name);
+        System.out.println("The Employee ID is : "+id);
+        System.out.println("The Employee salary is : "+salary);
+    }
+}
+class Main {
+    public static void main(String[] args)throws IOException {
+        System.out.println("Try programiz.pro");
+    }
+}
