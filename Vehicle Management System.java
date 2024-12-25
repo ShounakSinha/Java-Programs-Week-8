d8d89c5ed71f7d26862e88089441c1816da7cb8f
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-12-26 00:09:17 +0530

    Update Vehicle Management System.java

--- Vehicle Management System.java
+++ Vehicle Management System.java
@@ -10,11 +10,18 @@
 // Write a main() method to create and manipulate objects of Car and Bike.
 
 
+
 abstract class Vehicle
 {
     Vehicle()
     {
         System.out.println("Constructor of Vehicle Class called");
+    }
+    abstract void Start();
+    abstract void Stop();
+    public void display()
+    {
+        System.out.println("Hi...amra ekhon Vehicle er display te");
     }
 }
 class Car extends Vehicle
@@ -23,12 +30,28 @@ class Car extends Vehicle
     {
         System.out.println("Constructor of Car Class called");
     }
+    void Start()
+    {
+        System.out.println("The Cra is started");
+    }
+    void Stop()
+    {
+        System.out.println("The Cra is stopped");
+    }
 }
 class Bike extends Vehicle
 {
     Bike()
     {
         System.out.println("Constructor of Bike Class called");
+    }
+    void Start()
+    {
+        System.out.println("The Bike is started");
+    }
+    void Stop()
+    {
+        System.out.println("The Bike is stopped");
     }
 }
 class Main {
