4a4935b25a9955b358929d451a74135415d05e54
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-12-29 13:32:00 +0530

    Update Banking System.java

--- Banking System.java
+++ Banking System.java
@@ -1,1 +1,53 @@
+import java.io.*;
 
+class Bank
+{
+    Bank()
+    {
+        System.out.println("Banks constructor called");
+    }
+    public int getRateOfInterest()
+    {
+        
+    }
+}
+class SBI extends Bank
+{
+    SBI()
+    {
+        System.out.println("SBI's constructor called");
+    }
+    public int getRateOfInterest()
+    {
+        
+    }
+}
+class HDFC extends Bank
+{
+    HDFC()
+    {
+        System.out.println("HDFC Bank's constructor called");
+    }
+    public int getRateOfInterest()
+    {
+        
+    }
+}
+class ICICI extends Bank
+{
+    ICICI()
+    {
+        System.out.println("ICICI Bank's constructor called");
+    }
+    public int getRateOfInterest()
+    {
+        
+    }
+}
+
+class Main {
+    public static void main(String[] args)htrows IOException
+    {
+        
+    }
+}
